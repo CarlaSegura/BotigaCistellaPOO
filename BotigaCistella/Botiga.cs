41b9e2a296e18a7cc22c8e990cc09289dfc1fe25
Carla Segura <a2308761@institutmontilivi.cat>
2024-03-11 10:39:03 +0100

    Avançat

---
+++ BotigaCistella/Botiga.cs
@@ -0,0 +1,99 @@
+﻿namespace BotigaCistella
+{
+	public class Botiga
+	{
+        //ATRIBUTS
+        private string nomBotiga;
+        private int[] productes;
+        private int nElements;
+
+        //CONSTRUCTORS
+        public Botiga()
+		{
+            productes = new int[10];
+            nElements = 0;
+		}
+
+        public Botiga(string nomBotiga, int producte)
+        {
+            this.nomBotiga = nomBotiga;
+            productes = producte;
+        }
+
+        public Botiga():this()
+        {
+            
+        }
+
+        //PROPIETATS
+        public string NomBotiga
+        {
+            set { nomBotiga = value; }
+            get { return nomBotiga; }
+        }
+        public int[] Productes
+        {
+            set { productes = value; }
+            get { return productes; } 
+        }
+        public int NElements
+        {
+            set { nElements = value; }
+            get { return nElements; } 
+        }
+
+        //METODES
+        public int EspaiLliure()
+        {
+
+        }
+
+        public int AfegirProducte(Producte producte)
+        {
+
+        }
+
+        public int AfegirProducte(Producte[] productes)
+        {
+
+        }
+
+        public int AmpliarBotiga(int num)
+        {
+
+        }
+
+        public int ModificarPreu(string producte, double preu)
+        {
+
+        }
+
+        public int BuscarProducte(Producte producte)
+        {
+
+        }
+
+        public int ModificarProducte(Producte producte, String nouNom, double nouPreu, int novaQuantitat)
+        {
+
+        }
+
+        public int OrdenarProducte()
+        {
+
+        }
+        public int OrdenarPreus()
+        {
+
+        }
+        public int Mostrar()
+        {
+
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
+
--- BotigaCistella/Producte.cs
+++ BotigaCistella/Producte.cs
@@ -2,23 +2,41 @@
 {
 	public class Producte
 	{
-        //Atributs
+        //ATRIBUTS
         private string nom;
         private int preuSenseIva;
         private int iva;
         private int quantitat;
 
-        //Constructors
+        //CONSTRUCTORS
+
+        /// <summary>
+        /// 
+        /// </summary>
         public Producte()
 		{
             iva = 21;
             quantitat = 0;
 		}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nom"></param>
+        /// <param name="preuInicial"></param>
         public Producte(string nom, int preuInicial) : this()
         {
             this.nom = nom;
             preuSenseIva = preuInicial;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nom"></param>
+        /// <param name="preuInicial"></param>
+        /// <param name="iva"></param>
+        /// <param name="quantitat"></param>
         public Producte(string nom, int preuInicial, int iva, int quantitat)
         {
             this.nom = nom;
@@ -27,39 +45,48 @@
             this.quantitat = quantitat;
         }
 
-        //Propietats
-        //VALIDAR VALORS POSITIUS
+        //PROPIETATS
+
         public string Nom
         {
             set { nom = value; }
-            get { return nom; }
+            get { return nom; } //VALIDAR VALORS POSITIUS
         }
         public int PreuSenseIva
         {
             set { preuSenseIva = value; }
-            get { return preuSenseIva; }
+            get { return preuSenseIva; } //VALIDAR VALORS POSITIUS
         }
         public int Iva
         {
             set { iva = value; }
-            get { return iva; }
+            get { return iva; } //VALIDAR VALORS POSITIUS
         }
         public int Quantitat
         {
             set { quantitat = value; }
-            get { return quantitat; }
+            get { return quantitat; } //VALIDAR VALORS POSITIUS
         }
 
-        //Metodes (publics)
+        //METODES (PUBLICS)
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public int Preu()
         {
             int preu = iva / 100 + preuSenseIva;
             return preu;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
-            return ;
+            return $"Nom: {nom}\nPreu: {preuSenseIva}\nQuantitat: {quantitat}"; ;
         }
     }
 }
